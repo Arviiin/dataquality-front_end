5583d7750359a1a5f221ff61456dc6a34fc7962a
Arviiin <931639826@qq.com>
2019-02-25 16:21:26 +0800

    数据一览

--- src/views/dataprocess/databrowser.vue
+++ src/views/dataprocess/databrowser.vue
@@ -1,11 +1,13 @@
 <template>
   <div>
+
     <navHeader class="header" :userinfo="this.userinfo" :vlogin="this.vlogin"></navHeader>
     <hr>
+
     <div>
       <menuSide :vlogin="this.vlogin"></menuSide>
       <!--<el-form :model="userinfo" status-icon :rules="rules2" ref="userinfo" class="login-form">-->
-      <el-main style="background-color: #ffffff; height: 500px">
+      <el-main style="background-color: #ffffff; height: 350px">
         <div class="title-container">
           <h3 class="title">数据一览</h3>
         </div>
@@ -15,38 +17,25 @@
       </el-input>
         <br>
         <br>
+
         <el-table
-          v-if="this.diag==true"
           :data="tableData"
           style="width: 100%"
-          height="250">
+          v-if="this.diag==true"
+          :header-cell-style="{background:'#f5f7fa'}">
+          <template v-for="col in cols">
           <el-table-column
-            fixed
-            prop="date"
-            label="日期">
+            width="180"
+            :prop="col.prop" :label="col.lable" >
           </el-table-column>
-          <el-table-column
-            prop="name"
-            label="姓名">
-          </el-table-column>
-          <el-table-column
-            prop="province"
-            label="省份">
-          </el-table-column>
-          <el-table-column
-            prop="city"
-            label="市区">
-          </el-table-column>
-          <el-table-column
-            prop="address"
-            label="住址">
-          </el-table-column>
-          <el-table-column
-            prop="zip"
-            label="邮编">
-          </el-table-column>
+          </template>
         </el-table>
-        <el-pagination
+
+        <!--<el-button raw-type="button" @click="addCol">-->
+          <!--添加一列-->
+        <!--</el-button>-->
+
+        <el-pagination v-if="this.diag==true"
           background
           layout="prev, pager, next"
           :total="1000"
@@ -64,6 +53,8 @@
   import navFooter from '@/views/components/footer'
   import menuSide from '@/views/components/menuside'
 
+  let moment = require("moment");
+
   export default {
     name: "databrowser",
     components:{
@@ -79,57 +70,29 @@
         },
         input0:'',
         vlogin:this.$route.params.vlogin?this.$route.params.vlogin:'true',
-        tableData:
-          [{
-          date: '2016-05-03',
-          name: '庄计龙',
-          province: '上海',
-          city: '长宁区',
-          address: '东华大学',
-          zip: 200333
+        tableData: [{
+          date: '2016-05-02',
+          name: '王小虎',
+          address: '上海市普陀区金沙江路 1518 弄'
         }, {
-        date: '2016-05-02',
-          name: '猪猪侠',
-          province: '安徽',
-          city: '合肥市',
-          address: '合肥工业大学',
-          zip: 200333
-      }, {
-        date: '2016-05-04',
+          date: '2016-05-04',
           name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-      }, {
-        date: '2016-05-01',
-          name: '马云',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-      }, {
-        date: '2016-05-08',
-          name: '马化腾',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-      }, {
-        date: '2016-05-06',
+          address: '上海市普陀区金沙江路 1517 弄'
+        }, {
+          date: '2016-05-01',
           name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-      }, {
-        date: '2016-05-07',
+          address: '上海市普陀区金沙江路 1519 弄'
+        }, {
+          date: '2016-05-03',
           name: '王小虎',
-          province: '上海',
-          city: '普陀区',
-          address: '上海市普陀区金沙江路 1518 弄',
-          zip: 200333
-      }],
+          address: '上海市普陀区金沙江路 1516 弄'
+        }],
+        cols: [
+          {prop: 'date', lable: '日期'},
+          {prop: 'name', lable: '姓名'},
+        ]
+
+
       }
     },
     mounted:function(){
@@ -142,6 +105,32 @@
     methods:{
       search(tablename){
         console.log(tablename);
+        this.$ajax({
+          method: 'post',
+          url: 'http://localhost:8080/data/list',
+          params: {
+            tablename: tablename
+          }
+        }).then(response => {
+         console.log(response.data);
+         this.cols = response.data.result.clo;
+         this.tableData = response.data.result.tableData;
+         // this.cols = JSON.parse(this.cols)
+         this.tableData = JSON.parse(this.tableData)
+
+          for(var i=0;i<this.tableData.length;i++){
+            if(this.tableData[i].hasOwnProperty('createtime')){
+              this.tableData[i].createtime=moment(this.tableData[i].createtime).format('YYYY-MM-DD HH:mm:ss');
+            }
+            if(this.tableData[i].hasOwnProperty('updatetime')){
+              this.tableData[i].updatetime=moment(this.tableData[i].updatetime).format('YYYY-MM-DD HH:mm:ss');
+            }
+          }
+          console.log(this.cols);
+          console.log(this.tableData);
+        }).catch(function (err) {
+          //console.log(err);
+        })
         this.diag=true;
       }
     }
--- src/views/dataprocess/evaluation_report.vue
+++ src/views/dataprocess/evaluation_report.vue
@@ -4,10 +4,48 @@
     <hr>
     <div>
       <menuSide></menuSide>
-      <el-main style="background-color: #ffffff; height: 500px">
-        <div style="display: flex;height: 500px;width: 100%;align-items: center;justify-content: center;">
-            hhhh
-        </div>
+      <el-main style="background-color: #ffffff; height: 1000px">
+        <!--<div style="display: flex;height: 500px;width: 100%;align-items: center;justify-content: center;">-->
+          <el-table
+            :header-cell-style="{background:'#ccc'}"
+            :data="tableData"
+            style="width: 100%"
+            height="1000">
+            <el-table-column
+              fixed
+              prop="date"
+              label="评估模板标识">
+            </el-table-column>
+            <el-table-column
+              prop="name"
+              label="规则系列标识">
+            </el-table-column>
+            <el-table-column
+              prop="province"
+              label="总数量">
+            </el-table-column>
+            <el-table-column
+              prop="city"
+              label="合格数量">
+            </el-table-column>
+            <el-table-column
+              prop="address"
+              label="不符数量">
+            </el-table-column>
+            <el-table-column
+              prop="zip"
+              label="评估权重">
+            </el-table-column>
+            <el-table-column
+              prop="zip1"
+              label="评估得分">
+            </el-table-column>
+            <el-table-column
+              prop="date"
+              label="评估时间">
+            </el-table-column>
+          </el-table>
+        <!--</div>-->
       </el-main>
     </div>
   </div>
@@ -21,7 +59,62 @@
         components: {
           navHeader,
           menuSide,
+        },
+      data() {
+        return {
+          tableData: [{
+            date: '2016-05-03',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          }, {
+            date: '2016-05-02',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          }, {
+            date: '2016-05-04',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          }, {
+            date: '2016-05-01',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          }, {
+            date: '2016-05-08',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          }, {
+            date: '2016-05-06',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          }, {
+            date: '2016-05-07',
+            name: '王小虎',
+            province: '上海',
+            city: '普陀区',
+            address: '上海市普陀区金沙江路 1518 弄',
+            zip: 200333
+          }]
         }
+      }
+
     }
 </script>
 
